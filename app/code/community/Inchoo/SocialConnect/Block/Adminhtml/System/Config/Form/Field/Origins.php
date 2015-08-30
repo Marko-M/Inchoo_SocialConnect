ba1e608ca4f90dc7000135c4f3f70d2f4e604866
Marko Martinović <marko@techytalk.info>
2015-08-30 10:07:20 +0200

    Support secure admin URL in admin

--- app/code/community/Inchoo/SocialConnect/Block/Adminhtml/System/Config/Form/Field/Origins.php
+++ app/code/community/Inchoo/SocialConnect/Block/Adminhtml/System/Config/Form/Field/Origins.php
@@ -5,7 +5,10 @@ abstract class Inchoo_SocialConnect_Block_Adminhtml_System_Config_Form_Field_Ori
 {
 
     protected function _getElementHtml(Varien_Data_Form_Element_Abstract $element) {
-        return sprintf('<pre>%s</pre>', Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB));
+        return sprintf('<pre>%s</pre>',
+            Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_WEB),
+            $this->_isSecure()
+        );
     }
 
 }
